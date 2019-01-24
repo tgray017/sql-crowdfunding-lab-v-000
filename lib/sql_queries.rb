685fa2897df0fe1296b23e43c21586f7dffb1284
tgray017 <tgray017@gmail.com>
2019-01-24 12:28:23 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,10 +34,23 @@ GROUP BY
   1, 2
 ORDER BY
   u.name ASC;
-"end
+"
+end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"
+SELECT
+  pr.title,
+  u.age,
+  SUM(pl.amount) as pledge_total
+FROM
+  users u
+  JOIN pledges pl on u.id = pl.user_id
+GROUP BY
+  1, 2
+ORDER BY
+  u.name ASC;
+"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
