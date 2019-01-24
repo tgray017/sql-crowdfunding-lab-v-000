95c929bf686fd3221e14c98780aaf1bb10c7f240
tgray017 <tgray017@gmail.com>
2019-01-24 12:40:14 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,93 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+"
+SELECT
+  pr.title,
+  SUM(pl.amount) as pledge_total
+FROM
+  projects pr
+  JOIN pledges pl on pr.id = pl.project_id
+GROUP BY
+  1
+ORDER BY
+  pr.title ASC;
+"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"
+SELECT
+  u.name,
+  u.age,
+  SUM(pl.amount) as pledge_total
+FROM
+  users u
+  JOIN pledges pl on u.id = pl.user_id
+GROUP BY
+  1, 2
+ORDER BY
+  u.name ASC;
+"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"
+SELECT
+  pr.title,
+  (SUM(pl.amount) - pr.funding_goal) as amount_over_goal
+FROM
+  projects pr
+  JOIN pledges pl on pr.id = pl.project_id
+GROUP BY
+  pr.title
+HAVING
+  amount_over_goal >= 0
+ORDER BY
+  pr.title ASC;
+"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"
+SELECT
+  u.name,
+  SUM(pl.amount) as amount_pledged
+FROM
+  users u
+  JOIN pledges pl on u.id = pl.user_id
+GROUP BY
+  u.name
+ORDER BY
+  amount_pledged ASC, 
+  u.name ASC;
+"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"
+SELECT
+  pr.category,
+  pl.amount
+FROM
+  projects pr
+  JOIN pledges pl on pr.id = pl.project_id
+WHERE
+  pr.category = 'music'
+"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"
+SELECT
+  pr.category,
+  SUM(pl.amount) as amount_pledged
+FROM
+  projects pr
+  JOIN pledges pl on pr.id = pl.project_id
+WHERE
+  pr.category = 'books'
+GROUP BY
+  pr.category
+"
 end
